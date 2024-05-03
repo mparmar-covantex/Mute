9e067e8d604b594f9da0ae94d54cfdb214ca8636
Mehul Parmar <mparmar.mailbox@gmail.com>
2024-05-03 14:28:21 +0530

    Updated Package Condifuration

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.10
 
 import PackageDescription
 
 let package = Package(
     name: "Mute",
-    platforms: [.iOS(.v9)],
+	defaultLocalization: "en",
+	platforms: [
+		.iOS(.v13), .macOS(.v13), .watchOS(.v9), .tvOS(.v16)
+	],
     products: [.library(name: "Mute", targets: ["Mute"])],
     targets: [.target(name: "Mute", path: "Mute", resources: [.copy("Assets/mute.aiff")])]
 )
