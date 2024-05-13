0effc584d807dfb6e76c6ea6e0bba5059c1dd4fd
Mehul Parmar <m.parmar@covantex.com>
2024-05-13 16:25:14 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Mute",
 	defaultLocalization: "en",
 	platforms: [
-		.iOS(.v13), .macOS(.v13), .watchOS(.v9), .tvOS(.v16)
+		.iOS(.v13), .macOS(.v13)
 	],
     products: [.library(name: "Mute", targets: ["Mute"])],
     targets: [
